b83c460baa5da7be05003b9b265c77c984c00fcd
raj_NVN <rajNVN@users.noreply.github.com>
2021-08-15 00:26:47 +0530

    init spring-boot project for SHG

---
+++ src/main/java/com/rajCo/shg/ShgApplication.java
@@ -0,0 +1,13 @@
+package com.rajCo.shg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajCo/shg/ShgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajCo.shg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
